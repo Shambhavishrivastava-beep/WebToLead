e02e702b332e230dcf24d247d1cfe160f6446764
Shambhavishrivastava-beep <shambhavishrivastava23@gmail.com>
2026-02-18 20:35:39 +0530

    CaptchaChecked

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-function beforesubmit(){
+let captchachecked = false;
+function beforesubmit(event){
+    if(captchachecked){
     let inputdate=document.querySelector(".inputdate");
     let outputdate=document.querySelector(".outputdate");
     console.log("inputdate.value",inputdate.value);
     let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formattedDate;
+    }else{
+        alert("Please check the recaptcha box");
+        event.preventDefault();
+    }
 }
 
 function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function captchasuccess(){
+    captchachecked=true;
+}
+
